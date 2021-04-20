60b1ba702941395d5518c414a7818ae584c64ca6
Tridev Reddy <73326725+TridevReddy@users.noreply.github.com>
2021-04-20 09:37:22 +0530

    Update smsghost.py

--- smsghost.py
+++ smsghost.py
@@ -1,5 +1,5 @@
 import requests
-Country_code = input("Enter the code without + ------>")
+Country_code = input("Enter the code without + ------>") #Enter the code only. For example 91 or 92. Do not add '+'
 Mobile_Number = input("Enter the phone number ---------->")
 Receiver = "+" + Country_code + Mobile_Number
 Message = input("Enter message to be sent")
